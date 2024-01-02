ee35aa78f2b61057056d52b7105ce8e50cf953c7
Ryota KITA <snptkdn@snptkdn-2.local>
2024-01-03 01:06:35 +0900

    delete: input.tsx

--- components/signin/input.tsx
+++
@@ -1,7 +0,0 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
-export function InputWithLabel({ label, ...props }: { label: string }) {
-  return (
-  )
-}
