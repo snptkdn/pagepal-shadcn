9178e6c645ce92d9c9f40365c2de7d86c27ebe55
Ryota KITA <snptkdn@snptkdn-2.local>
2024-01-03 00:59:04 +0900

    add: first commit

---
+++ components/signin/input.tsx
@@ -0,0 +1,7 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function InputWithLabel({ label, ...props }: { label: string }) {
+  return (
+  )
+}
